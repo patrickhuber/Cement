8c3e382bb55dd56da269da0e3f943edbbcafdf88
Patrick Huber <prhuber@gmail.com>
2013-09-30 20:54:35 -0400

    adds some initialization work.

---
+++ Integration.Transforms.Designer.AutoMapper/IAutoMapperContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+
+namespace Integration.Transforms.Designer.AutoMapper
+{
+    public interface IAutoMapperContext
+    {        
+        IMappingEngine Engine { get; }
+        void Configure();
+    }
+}
