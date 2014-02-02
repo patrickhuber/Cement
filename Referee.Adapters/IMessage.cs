bbfae377490b12e28b9d01627403aed2054fab34
Patrick Huber <prhuber@gmail.com>
2014-02-02 17:39:37 -0500

    updates all names to Referee

---
+++ Referee.Adapters/IMessage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Referee.Adapters
+{
+    public interface IMessage
+    {
+        System.IO.Stream Body { get; set; }
+        IMessageHeader Header { get; set; }
+    }
+}
---
+++ Referee.Adapters/IMessageHeader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Referee.Adapters
+{
+    public interface IMessageHeader
+    {
+    }
+}
