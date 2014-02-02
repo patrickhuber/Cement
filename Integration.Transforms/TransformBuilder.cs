2376f414dfe8843f8e1dbd77837a353c8f589be3
Patrick Huber <prhuber@gmail.com>
2014-02-02 14:18:17 -0500

    adds comments to transform builder

--- Integration.Transforms/TransformBuilder.cs
+++ Integration.Transforms/TransformBuilder.cs
@@ -5,15 +5,32 @@ using System.Text;
 
 namespace Integration.Transforms
 {
+    /// <summary>
+    /// Defines a transform builder that can be used to create transforms with a fluent interface.
+    /// </summary>
     public class TransformBuilder
     {
+        /// <summary>
+        /// Gets or sets the transform.
+        /// </summary>
+        /// <value>
+        /// The transform.
+        /// </value>
         public Transform Transform { get; protected set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransformBuilder"/> class.
+        /// </summary>
         public TransformBuilder() 
         {
             Transform = new Transform();
         }
 
+        /// <summary>
+        /// Inputses the specified inputs.
+        /// </summary>
+        /// <param name="inputs">The inputs.</param>
+        /// <returns></returns>
         public TransformBuilder Inputs(Action<IObjectFactory<ModelBuilder>> inputs)
         {
             GenericObjectFactory<ModelBuilder> modelBuilderFactory = new GenericObjectFactory<ModelBuilder>();
@@ -22,6 +39,11 @@ namespace Integration.Transforms
             return this;
         }
 
+        /// <summary>
+        /// Outputses the specified outputs.
+        /// </summary>
+        /// <param name="outputs">The outputs.</param>
+        /// <returns></returns>
         public TransformBuilder Outputs(Action<IObjectFactory<ModelBuilder>> outputs)
         {
             GenericObjectFactory<ModelBuilder> modelBuilderFactory = new GenericObjectFactory<ModelBuilder>();
