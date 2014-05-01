2e31f8d18fedd6d9043ed033f6294a2150049011
Patrick Huber <prhuber@gmail.com>
2014-05-01 15:07:48 -0400

    adds async operations

---
+++ Cement.Core/Channels/IAsyncInputChannel.cs
@@ -0,0 +1,14 @@
+﻿using Cement.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cement.Channels
+{
+    public interface IAsyncInputChannel
+    {
+        Task ReceiveAsync(IMessage message);
+    }
+}
---
+++ Cement.Core/Channels/IAsyncOutputChannel.cs
@@ -0,0 +1,14 @@
+﻿using Cement.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cement.Channels
+{
+    public interface IAsyncOutputChannel
+    {
+        Task<IMessage> SendAsync();
+    }
+}
