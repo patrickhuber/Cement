7d42ed1540d03a6b0700451357aeeb4015c551ac
Patrick Huber <prhuber@gmail.com>
2014-06-15 15:26:27 -0400

    adds passthrough channel

---
+++ Libraries/Cement.Core/Channels/PassThroughChannel.cs
@@ -0,0 +1,39 @@
+﻿using Cement.Adapters;
+using Cement.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cement.Channels
+{
+    public class PassThroughChannel : IObservable<IMessage>, IObserver<IMessage>
+    {
+        protected Monitor<IMessage> messageMonitor;
+
+        public PassThroughChannel() { }
+
+        public void Send(IMessage message) { }
+
+        public IDisposable Subscribe(IObserver<IMessage> observer)
+        {
+            return messageMonitor.Subscribe(observer);
+        }
+
+        public void OnCompleted()
+        {
+            // clean up resources
+        }
+
+        public void OnError(Exception error)
+        {
+            // log errors
+        }
+
+        public void OnNext(IMessage value)
+        {
+            // process?
+        }
+    }
+}
---
+++ Tests/Cement.Core.Tests.Unit/Channels/PassThroughChannelTests.cs
@@ -0,0 +1,32 @@
+﻿using Cement.Adapters;
+using Cement.Channels;
+using Cement.Messages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cement.Tests.Unit.Channels
+{
+    [TestClass]
+    public class PassThroughChannelTests
+    {
+        public void Test_PassThroughChannel_Streams_Data()
+        {
+            var mockReceiveAdapter = new Mock<IReceiveAdapter>();
+            var mockSendAdapter = new Mock<ISendAdapter>();
+            
+            var receiveAdapter = mockReceiveAdapter.Object;
+            var sendAdapter = mockSendAdapter.Object;
+
+            // the sender creates a message and writes that message to the channel
+            // the channel has a list of receivers that it forwards the message to
+            var message = receiveAdapter.Receive();
+            var passThroughChannel = new PassThroughChannel();
+            passThroughChannel.Subscribe(receiveAdapter);
+        }
+    }
+}
