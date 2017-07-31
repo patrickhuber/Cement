de6185ba97eb4cc517417a29cd66a77d5b401254
patrick huber <prhuber@gmail.com>
2017-07-30 23:56:35 -0400

    adds null sink

---
+++ libraries/Cyrus/NullSink.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Cyrus
+{
+    public class NullSink : ISink
+    {
+        public IReceiveChannel ReceiveChannel { get; }
+
+        public NullSink(IReceiveChannel receiveChannel)
+        {
+            ReceiveChannel = receiveChannel;
+        }
+
+        public async Task SendAsync()
+        {
+            using (var message = ReceiveChannel.Receive())
+            {
+                var buffer = new byte[1024];
+                var bytesRead = 0;
+                do
+                {
+                    bytesRead = await message.ReadAsync(buffer, 0, buffer.Length);
+                } while (bytesRead > 0);
+            }
+        }
+
+    }
+}
